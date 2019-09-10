642beac4f2360094136d90f61474d4e9ab7fca62
charmingsong <web_songsong@163.com>
2019-09-10 21:22:10 +0800

    1111

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,5 +1,7 @@
+const isProduction = process.env.NODE_ENV === 'production'
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const nodeExternals = require('webpack-node-externals')
 const path = require('path')
 const merge = require('lodash.merge')
@@ -13,10 +15,25 @@ module.exports = {
   css: {
     extract: false
   },
-  configureWebpack: () => {
+  configureWebpack: config => {
+    let plugins = [
+      TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()
+    ]
+    if (isProduction) {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          // 正在匹配需要压缩的文件后缀
+          test: /\.(js|css|svg|woff|ttf|json|html)$/,
+          // 大于10kb的会压缩
+          threshold: 10240,
+          // 其余配置查看compression-webpack-plugin
+          deleteOriginalAssets: false
+        })
+      )
+    }
+
     return {
       entry: `./src/entry-${target}.js`,
-
       devtool: 'source-map',
       target: TARGET_NODE ? 'node' : 'web',
       node: false,
@@ -29,9 +46,7 @@ module.exports = {
             whitelist: [/\.css$/]
           })
         : undefined,
-      plugins: [
-        TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()
-      ]
+      plugins
     }
   },
   chainWebpack: config => {
@@ -49,5 +64,5 @@ module.exports = {
       })
   },
   filenameHashing: false,
-  indexPath: 'dev.html'
+  css: {}
 }
